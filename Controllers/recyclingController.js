c4f64aa17afb31d578186d647e7d3441ce924022
sahil58555 <sahil.saxena.58555@gmail.com>
2023-05-13 12:07:25 +0530

    search feature added

--- Controllers/recyclingController.js
+++ Controllers/recyclingController.js
@@ -2,7 +2,30 @@ const recyclingpart = require("../Models/PartsForRecycling");
 const aircraftpart = require("../Models/AircraftPartsDataTable");
 
 const getAircraftsStatus = async (req, res) => {
-  const aircrafts = await recyclingpart.find();
+  const query = {...req.query};
+  const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+  excludedFields.forEach(ele => delete query[ele]);
+
+  let queryDB = recyclingpart.find(query);
+
+  if(req.query.sort) {
+    queryDB = queryDB.sort(req.query.sort.split(',').join(' '));
+  }
+
+  if(req.query.fields) {
+    queryDB = queryDB.select(req.query.fields.split(',').join(' '));
+  }
+
+  if(req.query.page) {
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 5;
+    const skip = (page - 1) * limit;
+
+    queryDB = queryDB.skip(skip).limit(limit);
+  }
+
+  const aircrafts = await queryDB;
   res.status(200).json({
     status: "success",
     result: aircrafts.length,
