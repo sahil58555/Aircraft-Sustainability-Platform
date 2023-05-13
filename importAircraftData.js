53b840cfbc8e6957759b95002c7adfb633e81ad6
lokesh783 <lokesh.rahul2010@gmail.com>
2023-05-13 12:52:17 +0530

    dyn db

--- importAircraftData.js
+++ importAircraftData.js
@@ -1,24 +1,28 @@
+const axios = require('axios');
 const xlsx = require('xlsx');
 
-const fetchAircraftData = (limit) => {
+const fetchAircraftData = async (limit) => {
+  const fileUrl = 'https://docs.google.com/spreadsheets/d/1ANhv8wQzGBhu6mANUmmhaLCFQsEktc1M/export?format=xlsx';
 
-  const workbook = xlsx.readFile('aircraft_parts_data.xlsx');
-  const sheetName = workbook.SheetNames[0]; // assuming the data is in the first sheet
+  const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
+
+  const workbook = xlsx.read(response.data, { type: 'buffer' });
+  const sheetName = workbook.SheetNames[0]; 
   const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-  
-  const combinedKeysData = sheetData.map((row) => {
-      const newRow = {};
-      for (const key in row) {
-        if (Object.hasOwnProperty.call(row, key)) {
-          const newKey = key.charAt(0).toLowerCase() + key.slice(1);
-          newRow[newKey.replace(/\s/g, '')] = row[key];
-        }
-      }
-      return newRow;
-    });
-  
-  return combinedKeysData.slice(0, limit);
-}
 
+
+  const combinedKeysData = sheetData.map((row) => {
+    const newRow = {};
+    for (const key in row) {
+      if (Object.hasOwnProperty.call(row, key)) {
+        const newKey = key.charAt(0).toLowerCase() + key.slice(1);
+        newRow[newKey.replace(/\s/g, '')] = row[key];
+      }
+    }
+    return newRow;
+  });
+
+  return combinedKeysData.slice(0, limit);
+};
 
 module.exports = fetchAircraftData;
